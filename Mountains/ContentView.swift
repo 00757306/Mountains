22932112bb687e3ad415a149d3117b22395607d1
Carol <s110416045@stu.ntue.edu.tw>
2019-12-07 13:02:59 +0800

    Initial Commit

---
+++ Mountains/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mountains
+//
+//  Created by Ong Wei Yee on 2019/12/7.
+//  Copyright © 2019 The Mountains. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
